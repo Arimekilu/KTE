065e1f3e635059e753069e6439a81fb4ecc9fab9
Admin <smosdevelop@gmail.com>
2024-03-04 17:41:28 +0300

    Добавил навигацию

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: "full",
-    redirectTo: 'items'
+    component: AppComponent
   },
   {
     path: 'items',
